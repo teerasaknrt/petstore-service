23c23a36a1c11aa78dc9802cbb9c6070e905ab3e
Teerasak Narairam <teerasak.nr@gmail.com>
2020-03-09 15:06:38 +0700

    [✨] add serving swagger

--- pkg/protocol/rest/server.go
+++ pkg/protocol/rest/server.go
@@ -14,16 +14,28 @@ import (
 	"google.golang.org/grpc"
 )
 
+func serveSwagger(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "www/swagger.json")
+}
+
 // RunServer runs HTTP/REST gateway
 func RunServer(ctx context.Context, grpcPort, httpPort string) error {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	mux := runtime.NewServeMux()
+	rmux := runtime.NewServeMux()
 	opts := []grpc.DialOption{grpc.WithInsecure()}
-	if err := patstoreV1.RegisterPetStoreServiceHandlerFromEndpoint(ctx, mux, "0.0.0.0:"+grpcPort, opts); err != nil {
+	if err := patstoreV1.RegisterPetStoreServiceHandlerFromEndpoint(ctx, rmux, "0.0.0.0:"+grpcPort, opts); err != nil {
 		log.Fatalf("failed to start HTTP gateway: %v", err)
 	}
+
+	// Serve the swagger,
+	mux := http.NewServeMux()
+	mux.Handle("/", rmux)
+
+	mux.HandleFunc("/swagger.json", serveSwagger)
+	fs := http.FileServer(http.Dir("www/swagger-ui"))
+	mux.Handle("/swagger-ui/", http.StripPrefix("/swagger-ui", fs))
 
 	srv := &http.Server{
 		Addr:    ":" + httpPort,
@@ -45,5 +57,6 @@ func RunServer(ctx context.Context, grpcPort, httpPort string) error {
 	}()
 
 	log.Println("starting HTTP/REST gateway..." + httpPort)
+	log.Println("Serving Swagger at: http://localhost:8080/swagger-ui/")
 	return srv.ListenAndServe()
 }
